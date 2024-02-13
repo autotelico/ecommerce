eee3d4894770be743a1e5a0d17ad070dbb6c9b01
Henrique Heron <henriqueheron1@gmail.com>
2024-02-13 09:06:30 -0300

    Build logic for item addition and deletion

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
+import CurrencyInput from "react-currency-input-field";
 
 interface Item {
+  id: number;
   name: string;
-  price: number;
-  amount: number;
+  price: string;
+  amount: string;
 }
 
 export default function Catalog() {
-  const [itemList, setItemList] = useState([]);
+  const [itemList, setItemList] = useState<Item[]>([]);
   const [showItemMenu, setShowItemMenu] = useState(false);
+  const idRef = useRef(0);
 
   function handleShow() {
     setShowItemMenu(!showItemMenu);
@@ -19,15 +22,21 @@ export default function Catalog() {
     const itemPrice = document.querySelector<HTMLInputElement>("#item-price");
     const itemAmount = document.querySelector<HTMLInputElement>("#item-amount");
 
-    const item = {
-      name: itemName?.value,
-      price: itemPrice?.value,
-      amount: itemAmount?.value,
+    const item: Item = {
+      id: idRef.current,
+      name: itemName?.value || "",
+      price: itemPrice?.value || "",
+      amount: itemAmount?.value || "",
     };
 
-    setItemList((prevItemList: Item[]) => [...prevItemList, item]);
+    setItemList((prevItemList) => [...prevItemList, item]);
+    idRef.current++;
     handleShow();
     console.log(itemList);
+  }
+
+  function deleteItem(itemId: number) {
+    setItemList(itemList.filter(item => item.id !== itemId))
   }
 
   return (
@@ -37,22 +46,24 @@ export default function Catalog() {
         handleShow={handleShow}
         addItem={addItem}
       />
-      {!!itemList.length && <Item itemList={itemList} />}
-      {/* temporary */}
-      <Item itemList={itemList} />
+      {!!itemList.length && <Item itemList={itemList} deleteItem={deleteItem}/>}
     </div>
   );
 }
 
-function Item({ itemList }) {
+function Item({ itemList, deleteItem }) {
   return (
     <>
       {itemList.map((item: Item) => {
         return (
-          <div className="item">
-            <p>{item.name}a</p>
-            <p>{item.price}</p>
-            <p>{item.amount}</p>
+          <div key={item.id} className="item">
+            <p>{item.name}</p>
+            <p>
+              Price:{" "}
+              {item.price.includes(",") ? item.price : item.price + ",00"}
+            </p>
+            <p>Available amount: {item.amount}</p>
+            <button onClick={() => deleteItem(item.id)}>Delete item</button>
           </div>
         );
       })}
@@ -65,9 +76,15 @@ function ItemMenu({ showItemMenu, handleShow, addItem }) {
     <>
       {showItemMenu ? (
         <div id="item-menu">
-          <input type="text" id="item-name" />
-          <input type="text" id="item-price" />
-          <input type="text" id="item-amount" />
+          <input type="text" id="item-name" placeholder="Item name" />
+          <CurrencyInput
+            id="item-price"
+            placeholder="Price"
+            prefix="R$ "
+            decimalsLimit={2}
+            maxLength={6}
+          />
+          <input type="text" id="item-amount" placeholder="Available amount" />
           <button onClick={addItem}>Add</button>
           <button onClick={handleShow}>Back</button>
         </div>
